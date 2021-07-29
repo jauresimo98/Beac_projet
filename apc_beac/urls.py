410aa27526c6bfb8caac46787e13b203a34b38d3
unknown <mlionelstephen@gmail.com>
2021-07-29 13:07:05 +0100

    vue secretaire

--- apc_beac/urls.py
+++ apc_beac/urls.py
@@ -3,13 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-<<<<<<< HEAD
-
-=======
     path('input', views.simple_upload, name='input'),
     path('periode', views.periode, name='periode'),
     path('recupere_periode', views.recupere_periode, name='recupere_periode'),
-    
->>>>>>> ed8ede5b02c958b978456b483604be6b6a1db5c8
 
 ]
